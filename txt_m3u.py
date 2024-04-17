d4ce86a623650a2c8ad12d6184cb22cf646c9e2e
lucheng7996 <lucheng7996@163.com>
2024-04-17 17:07:08 +0800

    Update txt_m3u.py

--- txt_m3u.py
+++ txt_m3u.py
@@ -1,19 +1,34 @@
-def txt_to_m3u():   
+def txt_to_m3u(input_file, output_file):
+    # 读取txt文件内容
+    with open(input_file, 'r', encoding='utf-8') as fr:
+        lines = fr.readlines()
 
-   # 打开文件并获取文件对象
-    file = open("IPTV_Z.m3u", "w")
-    
-    # 写入单行文本
-    file.write("Hello, world!\n")
-    
-    # 写入多行文本
-    lines = ["This is line 1\n", "This is line 2\n", "This is line 3\n"]
-    file.writelines(lines)
-    
-    # 关闭文件
-    file.close()
+    # 打开m3u文件并写入内容
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.write('#EXTM3U\n')
+        print(f"写入文件开始。")
+        # 初始化genre变量
+        genre = ''
+
+        # 遍历txt文件内容
+        for line in lines:
+            line = line.strip()
+            if line:
+                # 检查是否是genre行
+                channel_name, channel_url = line.split(',', 1)
+                if channel_url == '#genre#':
+                    genre = channel_name
+                    print(genre)
+                else:
+                    # 将频道信息写入m3u文件
+                    f.write(f'#EXTINF:-1 group-title="{genre}",{channel_name}\n')
+                    f.write(f'{channel_url}\n')
+        print(f"写入文件结束。")
+
+    fr.close()
+    f.close()
 
 # 将txt文件转换为m3u文件
-txt_to_m3u()
+txt_to_m3u('IPTV_ZB.txt', 'IPTV_Z.m3u')
 
 print(f"m3u文件创建成功,IPTV_Z.m3u")
