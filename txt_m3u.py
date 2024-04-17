7c37f9e7fbd65c27eeef87e3337a19773d07a4fc
lucheng7996 <lucheng7996@163.com>
2024-04-17 16:46:31 +0800

    Update txt_m3u.py

--- txt_m3u.py
+++ txt_m3u.py
@@ -5,7 +5,10 @@ def txt_to_m3u():
     with open('IPTV_Z.m3u', 'w', encoding='utf-8') as f:
         f.write('#EXTM3U\n')
         print(f"写入文件开始。")
-        f.write(f'#EXTINF:-1 group-title="AAA","http://kuanglong.vicp.hk:2121/udp/239.93.16.118:8708"\n')
+        f.write("TEST")
+        f.write("TEST")
+        f.write("TEST")
+
         # 初始化genre变量
         #genre = ''
 
