3b7368a812975b806506a536dbf42cb1b47a9de7
lucheng7996 <lucheng7996@163.com>
2024-04-17 15:38:18 +0800

    Update iptv-zb.py

--- iptv-zb.py
+++ iptv-zb.py
@@ -363,7 +363,6 @@ def txt_to_m3u(input_file, output_file):
         # 遍历txt文件内容
         for line in lines:
             line = line.strip()
-            # if "," in line: # 防止文件里面缺失“,”号报错
             if line:
                 # 检查是否是genre行
                 channel_name, channel_url = line.split(',', 1)
@@ -374,6 +373,7 @@ def txt_to_m3u(input_file, output_file):
                     # 将频道信息写入m3u文件
                     f.write(f'#EXTINF:-1 group-title="{genre}",{channel_name}\n')
                     f.write(f'{channel_url}\n')
+
 
 # 将txt文件转换为m3u文件
 txt_to_m3u('IPTV_ZB.txt', 'IPTV_ZB.m3u')
