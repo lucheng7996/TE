77726a5b88e381b4988d8471ac6a4cb8483e3763
lucheng7996 <lucheng7996@163.com>
2024-04-17 15:59:22 +0800

    Create txt_m3u.py

---
+++ txt_m3u.py
@@ -0,0 +1,31 @@
+def txt_to_m3u(input_file, output_file):
+    # 读取txt文件内容
+    with open(input_file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    # 打开m3u文件并写入内容
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.write('#EXTM3U\n')
+        print(f"写入文件开始。")
+        # 初始化genre变量
+        genre = ''
+
+        # 遍历txt文件内容
+        for line in lines:
+            line = line.strip()
+            if line:
+                # 检查是否是genre行
+                channel_name, channel_url = line.split(',', 1)
+                if channel_url == '#genre#':
+                    genre = channel_name
+                    print(genre)
+                else:
+                    # 将频道信息写入m3u文件
+                    f.write(f'#EXTINF:-1 group-title="{genre}",{channel_name}\n')
+                    f.write(f'{channel_url}\n')                    
+        print(f"写入文件结束。")
+                
+# 将txt文件转换为m3u文件
+txt_to_m3u('IPTV_ZB.txt', 'IPTV_ZB.m3u')
+
+print(f"m3u文件创建成功,IPTV_ZB.m3u")
